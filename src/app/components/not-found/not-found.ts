e73b5cec1fce5ff14da423df7d98e5e188e1aabd
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-06-10 16:01:56 +0200

    feat: add 404

--- src/app/components/not-found/not-found.ts
+++ src/app/components/not-found/not-found.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss'
 })
